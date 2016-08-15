afd3551da1804c6aa86c68d84737d5f8ce710cbb
muthu n. krishnan <mkrishnan@JA.COM>
2016-08-16 02:31:03 +0530

    yes

--- ShareImmediate/ShareImmediate/Controllers/PayController.cs
+++ ShareImmediate/ShareImmediate/Controllers/PayController.cs
@@ -16,9 +16,28 @@ namespace ShareImmediate.Controllers
         {
             CommonEntities db = new CommonEntities();
             var ViewModel = new ViewModels.PayVM();
+            ViewModel.AvailableUsers = (from r in db.Users.AsEnumerable() select r).ToList();
             
-            ViewBag.UserName = new SelectList(db.Users, "id", "name");
+            //ViewBag.UserName = new SelectList(db.Users, "id", "name");
             return View(ViewModel);
         }
+        [HttpPost]
+        public ActionResult Index(PayVM model)
+        {
+            CommonEntities db = new CommonEntities();
+            var ViewModel = new ViewModels.PayVM();
+            var NewPayment = new DataEntities.Payment();
+            foreach(var row in model.SelectedUser)
+            {
+                NewPayment.UserId = Int32.Parse(row);
+                NewPayment.Amount = model.PayAmount;
+                db.Payment.Add(NewPayment);
+                db.SaveChanges();
+            }
+            ViewModel.AvailableUsers = (from r in db.Users.AsEnumerable() select r).ToList();
+
+            return View("Index", ViewModel);
+        }
+
     }
 }
--- ShareImmediate/ShareImmediate/ViewModels/PayVM.cs
+++ ShareImmediate/ShareImmediate/ViewModels/PayVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ShareImmediate.ViewModels
 {
@@ -12,8 +13,10 @@ namespace ShareImmediate.ViewModels
         [Required]
         [Display(Name = "Amount")]
         public int PayAmount { get; set; }
+        public string[] SelectedUser { get; set; }
 
-        public string User { get; set; }
+
+        //public string User { get; set; }
 
         public IEnumerable<Users> AvailableUsers { get; set; }
 
